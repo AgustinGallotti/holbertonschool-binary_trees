f8e9ca59779540255b2c2ee266bd87b32e3de1fa
AgustinGallotti <Agustingallotti23@gmail.com>
2022-07-20 08:55:55 -0700

    task 13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -5,6 +5,7 @@
 * one child in a binary tree
 * @tree: is a pointer to the root node of the tree
 * to count the number of nodes
+* Return: count
 */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {

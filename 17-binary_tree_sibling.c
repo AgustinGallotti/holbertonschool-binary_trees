a63b6fe9a6a0b4a569066a84a2e059bb10a11a3c
AgustinGallotti <Agustingallotti23@gmail.com>
2022-07-20 09:03:40 -0700

    fix task 17 and 18

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -2,13 +2,14 @@
 
 /**
 * binary_tree_sibling - finds the sibling of a node
-* @tree to fin the sibling
+* @node: to find the sibling
+* Return: null or sibling node
 */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 	if (node == NULL || node->parent == NULL ||
 		node->parent->left == NULL || node->parent->right == NULL)
-			return (NULL);
+		return (NULL);
 	if (node->parent->left == node)
 		return (node->parent->right);
 	if (node->parent->right == node)
--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -2,16 +2,17 @@
 
 /**
 * binary_tree_uncle - finds the uncle of a node
-* @tree: is a pointer ot the node to find the uncle
+* @node: is a pointer ot the node to find the uncle
+* Return: null, or uncle of a node
 */
-binary_tree_t *binary_tree_uncle(binary_tree_t * node)
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	if (node == NULL || node->parent == NULL)
 		return (NULL);
 	node = node->parent;
 	if (node->parent == NULL || node->parent->left == NULL
 		|| node->parent->right == NULL)
-			return (NULL);
+		return (NULL);
 	if (node->parent->left == node)
 		return (node->parent->right);
 	if (node->parent->right == node)
